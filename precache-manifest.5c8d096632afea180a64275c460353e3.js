3b59d546783384d496fe623e0a3fb40ec12bc093
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-02-19 13:29:35 +0600

    Updates

---
+++ precache-manifest.5c8d096632afea180a64275c460353e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a8c6af989b4d117131a",
+    "url": "/react_todoapp/static/js/runtime~main.1a8c6af9.js"
+  },
+  {
+    "revision": "78204aa8f7ffadf189b5",
+    "url": "/react_todoapp/static/js/main.78204aa8.chunk.js"
+  },
+  {
+    "revision": "f13132071e5c2ed1d23c",
+    "url": "/react_todoapp/static/js/2.f1313207.chunk.js"
+  },
+  {
+    "revision": "78204aa8f7ffadf189b5",
+    "url": "/react_todoapp/static/css/main.5bd5add5.chunk.css"
+  },
+  {
+    "revision": "3f000035b52d3f47e7ec675f3b7d6bb7",
+    "url": "/react_todoapp/index.html"
+  }
+];
